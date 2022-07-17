e90cdf38c2edc46a1bd4798e8d213ea0b6f63825
Eric Cheng <60344483+BrineYT@users.noreply.github.com>
2022-07-17 16:35:59 +0800

    Add ping
    whatever this is for, it's here.

--- bot.py
+++ bot.py
@@ -22,12 +22,16 @@ async def on_member_remove(member):
     channel = bot.get_channel(993165929330507910)
     await channel.send(f"Bye there, @{member}.")
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+# @bot.event
+# async def on_message(message):
+#     if message.author == bot.user:
+#         return
 
-    await message.channel.send("Hello there!")
+#     await message.channel.send("Hello there!")
+
+@bot.command()
+async def ping(context):
+    await context.send(f"{round(bot.latency * 1000)} ms.")
 
 tokenFile = open("/home/brine/Codes/Python/token.txt", 'r')
 TOKEN = tokenFile.readline()
